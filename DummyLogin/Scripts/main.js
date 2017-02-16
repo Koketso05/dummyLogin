86778d2124219b2af08e735152788a7dfac85d46
julias <julias@delter.co.za>
2017-02-16 14:58:53 +0200

    Add project files.

---
+++ DummyLogin/Scripts/main.js
@@ -0,0 +1,71 @@
+﻿$().ready(function () {
+
+    HideLoader();
+
+    function HideForm()
+    {
+        $("#loginForm").hide();
+    }
+
+    function ShowLoader()
+    {
+        $("#loader").show();
+    }
+
+    function HideLoader() {
+        $("#loader").hide();
+    }
+
+    $('#login').click(function () {
+
+        var login = new Object();
+
+        login.Username = $("#username").val();
+        login.Password = $("#password").val();
+        var jLogin = JSON.stringify(login);
+
+        HideForm();
+
+        $.ajax({
+            url: "login.asmx/getUser",
+            //   url: "http://api.apixu.com/v1/current.json?key=3b6ea19016194bb8905122956171402&q=pretoria",
+            contentType: "application/json; charset=utf-8",                                       
+            method: "Get",
+            dataType: 'json',                    
+            data: { login: jLogin },
+            beforeSend: function(){
+                ShowLoader();
+            },
+            conplete: function (jqXHR, textStatus) {
+                console.log(jqXHR);
+                HideLoader();
+            },
+            success: function(response)
+            {
+                var _data = JSON.parse(response.d);
+                console.log(_data);
+                HideLoader();
+
+                if (_data.Success)
+                {                          
+                    $("#isUserValid").text("username and password valid");
+                    $("#isUserValid").css({ 'color': 'green', 'font-size': '150%' });
+                    $("#loginForm").hide();
+                }
+                else
+                {
+                    $("#isUserValid").text("username and password invalid");
+                    $("#isUserValid").css({ 'color': 'red', 'font-size': '150%' });
+                    $("#username").css({ 'border-color': 'red' });
+                    $("#password").css({ 'border-color': 'red' });
+                }                        
+            },
+            error: function(err)
+            {                        
+                console.log(err);
+                $("#isUserValid").text("An error occured. Please contact the webmaster");
+            }
+
+        });
+    });
+});
---
+++ DummyLogin/login.asmx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Script.Services;
+using System.Net.Mail;
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+
+namespace DummyLogin
+{
+    /// <summary>
+    /// Summary description for login
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/webmethods")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class login : System.Web.Services.WebService
+    {
+        [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public string getUser(user login)
+        {
+            Response response = new Response();
+
+            var cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("spUserLogin", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parUsername = new SqlParameter();
+            parUsername.ParameterName = "@Username";
+            parUsername.Value = login.Username;
+            cmd.Parameters.Add(parUsername);
+
+            SqlParameter parPassword = new SqlParameter();
+            parPassword.ParameterName = "@Password";
+            parPassword.Value = login.Password;
+            cmd.Parameters.Add(parPassword);
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            var harRows = sdr.HasRows;
+
+            if (harRows)
+            {
+                response.Code = 200;
+                response.Message = "execution was successful";
+                response.Success = true;                
+            }                
+            else
+            {
+                response.Code = 400;
+                response.Message = "execution was not successful";
+                response.Success = false;                
+            }
+            return new JavaScriptSerializer().Serialize(response);
+        }
+
+        [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = false)]        
+        public string sendMail()
+        {
+            try
+            {
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new System.Net.NetworkCredential("phahle.koketso", "200306351");
+                smtpServer.EnableSsl = true;
+
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress("julias@delter.co.za");
+                mail.To.Add("phahle.koketso@gmail.com");
+                mail.Subject = "Request to access webservice";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+                smtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                var errMess = ex.Message;
+                return errMess;
+            }
+            return "yes";
+        }
+    }
+}
